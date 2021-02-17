cc969b12a36bd89fdd48d6ce7e941e9d40b454c7
zetecha <79181923+zetecha@users.noreply.github.com>
2021-02-18 08:43:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,15 +12,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@client.event
-async def on_message(message):
-    if message.content == '/cleanup':
-        if message.author.guild_permissions.administrator:
-            await message.channel.purge()
-            await message.channel.send('抹消完了！！')
-        else:
-            await message.channel.send('何様のつもり？')
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
