893d61bd8f24f41d89d28a36b26480257a552076
zetecha <79181923+zetecha@users.noreply.github.com>
2021-02-18 21:53:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,10 +8,7 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-async def reply(message):
- reply = f'{message.author.mention} が通話に参加したよ！'
- await message.channel.send(reply)
-    
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -20,12 +17,12 @@ async def on_command_error(ctx, error):
     
 @client.event 
 async def on_voice_state_update(before, after): 
+     await ctx.send('pong')
         if after.server.id == SERVER_ID: 
         nowtime = datetime.datetime.utcnow() 
         nowtime = nowtime + datetime.timedelta(hours=9) 
         nowtime = nowtime.strftime("%m/%d-%H:%M") 
-        vcchannel = client.get_channel('通知させたいテキストチャンネルid') 
- 
+        vcchannel = client.get_channel(CHANNEL_ID) 
         if(before.voice_channel is None): 
             jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。" 
             await client.send_message(vcchannel, jointext) 
